c8b227a70fc7268480187f22d3818bfe6fd237a4
Nergül <nergulkahya019@gmail.com>
2022-09-30 18:09:34 +0300

    add pages view

--- Hotel_WebSite/Controllers/HomeController.cs
+++ Hotel_WebSite/Controllers/HomeController.cs
@@ -16,6 +16,22 @@ namespace Hotel_WebSite.Controllers
         {
             return View();
         }
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+        public IActionResult Services()
+        {
+            return View();
+        }
+        public IActionResult Rooms()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
